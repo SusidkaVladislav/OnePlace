092d5f113e1a69aca804925cc4230c4ad2f8de74
ViktoriaSadivska <vika.sadivska@gmail.com>
2023-07-05 23:26:22 +0300

    added OnePlace.DAL, added classes for all the entities

---
+++ OnePlace.DAL/Entities/Delivery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePlace.DAL.Entities
+{
+    internal class Delivery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PictureId { get; set; }
+        public Picture Picture { get; set; } 
+    }
+}
---
+++ OnePlace.DAL/Entities/Picture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePlace.DAL.Entities
+{
+    internal class Picture
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+    }
+}
