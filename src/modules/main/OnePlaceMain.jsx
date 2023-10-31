2b18154b8f88a178cc85a0dfe01fe5f0a982bfa6
ViktoriaSadivska <vika.sadivska@gmail.com>
2023-10-31 14:07:39 +0200

    added Banner1 component to the main page

--- src/modules/main/OnePlaceMain.jsx
+++ src/modules/main/OnePlaceMain.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Banner1 from './components/banner1/Banner1';
 
 const OnePlaceMain = () => {
     return (
+        <div>
         <Header/>
+        <Banner1/>
+        </div>
     )
 }
 
