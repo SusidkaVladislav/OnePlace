358a70c970ee01d6398cd08d9e5d0c6f5523a97e
ViktoriaSadivska <vika.sadivska@gmail.com>
2023-11-03 17:56:22 +0200

    added specialOffers component

--- src/modules/main/OnePlaceMain.jsx
+++ src/modules/main/OnePlaceMain.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header/Header';
 import Banner1 from './components/banner1/Banner1';
 import Banner2 from './components/banner2/Banner2';
+import SpecialOffers from './components/special_offers/SpecialOffers';
 
 const OnePlaceMain = () => {
     return (
@@ -9,6 +10,7 @@ const OnePlaceMain = () => {
         <Header/>
         <Banner1/>
         <Banner2/>
+        <SpecialOffers/>
         </div>
     )
 }
